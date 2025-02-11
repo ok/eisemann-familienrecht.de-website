8cbe7713bd99ff5bcd9cb4ad5a11cf9c232eea28
oliver <o.kohl@sap.com>
2025-02-11 23:13:48 +0100

    navbar mobile fix
    - fix for misaligned bruger menu on mobile
    - contact page added
    - canvas 0 padding for mobile

--- src/_data/site.js
+++ src/_data/site.js
@@ -7,7 +7,7 @@ export default {
   title: "Fachanwältin für Familienrecht - Kanzlei Eisemann",
   description: "Fachanwältin für Familienrecht in Berlin. Spezialisiert auf Scheidung, Unterhalt, Sorgerecht und Eheverträge. Persönliche und kompetente Beratung.",
   keywords: "Familienrecht Berlin, Fachanwältin für Familienrecht, Scheidungsanwalt Berlin, Unterhaltsrecht Berlin, Sorgerecht Anwalt Berlin, Umgangsrecht Beratung, Ehevertrag Berlin, Trennungsvereinbarung, Scheidungsfolgenvereinbarung, Vermögensauseinandersetzung, Zugewinnausgleich, Vaterschaftsanfechtung, Familienrechtliche Beratung, Familienanwalt Charlottenburg, Familienrechtliche Mediation",
-  domain: isDevelopment ? "http://localhost:8080" : "https://eisemann-familienrecht.de/",
+  domain: isDevelopment ? "http://localhost:8081" : "https://eisemann-familienrecht.netlify.app/",
   environment: process.env.NODE_ENV || "development",
   instaUrl: "",
   facebookUrl: "",
--- src/example.md
+++
@@ -1,7 +0,0 @@
----
-title: Contact
-layout: base.liquid
----
-    <section class="content">
-      <p class="is-size-4">Contact Page</p>
-    </section>
